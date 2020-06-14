3b3dc30873a12a737b4eec75f3ae3e94dadb3144
Lily Akhmetova <lilyakhmetovamail@gmail.com>
2020-06-14 21:51:51 +0300

    booking desktop

--- js/booking.js
+++ js/booking.js
@@ -2,3 +2,8 @@ document.getElementById('booking').onclick = function() {
 document.querySelector('body').style.background = '#9F57FF'
 document.getElementById('booking').innerHTML = 'СТОЛ ЗАБРОНИРОВАН'
 }
+
+document.getElementById('booking2').onclick = function() {
+document.querySelector('body').style.background = '#9F57FF'
+document.getElementById('booking2').innerHTML = 'СТОЛ ЗАБРОНИРОВАН'
+}
