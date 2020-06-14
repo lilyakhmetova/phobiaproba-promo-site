768f1a0c7a7ef1847ce1e91b8f937cd22e286dd1
Lily Akhmetova <lilyakhmetovamail@gmail.com>
2020-06-14 21:10:25 +0300

    booking mobile + tablet

---
+++ js/booking.js
@@ -0,0 +1,4 @@
+document.getElementById('booking').onclick = function() {
+document.querySelector('body').style.background = '#9F57FF'
+document.getElementById('booking').innerHTML = 'СТОЛ ЗАБРОНИРОВАН'
+}
