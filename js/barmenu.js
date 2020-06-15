9f32406eb6756ed341b029f6eab6fdc46318948a
Lily Akhmetova <lilyakhmetovamail@gmail.com>
2020-06-15 22:21:52 +0300

    barmenu mobile

---
+++ js/barmenu.js
@@ -0,0 +1,82 @@
+document.querySelector('.arachnophobia').onclick = function() {
+document.querySelector('.naturefear').innerHTML = 'арахнофобия (от&nbsp;др.-греч. &laquo;паук&raquo;, др. греч. &laquo;страх&raquo;)&nbsp;&mdash; частный случай зоофобии, боязнь паукообразных, относится к&nbsp;числу самых распространённых фобий. людей, страдающих арахнофобией, называют арахнофобами.'
+document.querySelector('.naturefear').style.marginTop = '24vw'
+document.querySelector('.spider').style.display = 'block'
+document.querySelector('.dog').style.display = 'none'
+document.querySelector('.snake').style.display = 'none'
+}
+
+document.querySelector('.kinophobia').onclick = function() {
+document.querySelector('.naturefear').innerHTML = 'кинофобия (от&nbsp;др.-греч. &laquo;собака&raquo; и&nbsp;др.-греч. &laquo;страх&raquo;)&nbsp;&mdash; психическое расстройство, фобия, объектом которой являются собаки.&nbsp;в&nbsp;практике психиатрии принято относить к&nbsp;кинофобии отдельные строго говоря фобии: страх покусов (адактофобия) и&nbsp;страх заражения бешенством (рабиефобия).'
+document.querySelector('.naturefear').style.marginTop = '14vw'
+document.querySelector('.dog').style.display = 'block'
+document.querySelector('.spider').style.display = 'none'
+document.querySelector('.snake').style.display = 'none'
+}
+
+document.querySelector('.ophiophobia').onclick = function() {
+document.querySelector('.naturefear').innerHTML = 'к&nbsp;животным, вызывающим у&nbsp;людей особенно сильный страх, несомненно, относятся разнообразные змеи. змеи во&nbsp;все времена играли значительную роль в&nbsp;сказаниях и&nbsp;верованиях народов, которые упоминают о&nbsp;змеях то&nbsp;со&nbsp;страхом и&nbsp;отвращением, то&nbsp;с&nbsp;любовью и&nbsp;благоговением.'
+document.querySelector('.naturefear').style.marginTop = '14vw'
+document.querySelector('.snake').style.display = 'block'
+document.querySelector('.dog').style.display = 'none'
+document.querySelector('.spider').style.display = 'none'
+}
+
+document.querySelector('.tanatophobia').onclick = function() {
+document.querySelector('.socialfear').innerHTML = 'страх смерти (танатофобия)&nbsp;&mdash; это тревога, вызванная мыслями о&nbsp;смерти, о&nbsp;прекращении существования (себя, или своих близких).'
+document.querySelector('.socialfear').style.marginTop = '20vw'
+document.querySelector('.death').style.display = 'block'
+document.querySelector('.death').style.marginBottom= '9vw'
+document.querySelector('.war').style.display = 'none'
+}
+
+document.querySelector('.warfear').onclick = function() {
+document.querySelector('.socialfear').innerHTML = 'страх войны входит в&nbsp;топ-5 самых сильных страхов у&nbsp;жителей россии. часто он&nbsp;подсознательный и&nbsp;иррациональный.'
+document.querySelector('.socialfear').style.marginTop = '20vw'
+document.querySelector('.war').style.display = 'block'
+document.querySelector('.war').style.marginBottom= '9vw'
+document.querySelector('.death').style.display = 'none'
+}
+
+document.querySelector('.acrophobia').onclick = function() {
+document.querySelector('.spacefear').innerHTML = 'акрофобия&nbsp;&mdash; навязчивый страх высоты. при нахождении на&nbsp;высоте у&nbsp;страдающего акрофобией наблюдается тошнота и&nbsp;головокружение.'
+document.querySelector('.spacefear').style.marginTop = '18vw'
+document.querySelector('.height').style.display = 'block'
+document.querySelector('.height').style.marginBottom= '9vw'
+document.querySelector('.field').style.display = 'none'
+document.querySelector('.cube').style.display = 'none'
+}
+
+document.querySelector('.agoraphobia').onclick = function() {
+document.querySelector('.spacefear').innerHTML = 'агорафобия выражается в&nbsp;боязни перед открытым пространством. индивид испытывает настоящий ужас от&nbsp;перспективы просто выйти на&nbsp;улицу.'
+document.querySelector('.spacefear').style.marginTop = '14vw'
+document.querySelector('.field').style.display = 'block'
+document.querySelector('.field').style.marginBottom= '9vw'
+document.querySelector('.height').style.display = 'none'
+document.querySelector('.cube').style.display = 'none'
+}
+
+document.querySelector('.claustrophobia').onclick = function() {
+document.querySelector('.spacefear').innerHTML = 'это психическое расстройство, выражающееся в&nbsp;сильной и&nbsp;патологической боязни закрытых или тесных пространств.'
+document.querySelector('.spacefear').style.marginTop = '18vw'
+document.querySelector('.cube').style.display = 'block'
+document.querySelector('.cube').style.marginBottom= '9vw'
+document.querySelector('.field').style.display = 'none'
+document.querySelector('.height').style.display = 'none'
+}
+
+document.querySelector('.niktophobia').onclick = function() {
+document.querySelector('.insidefear').innerHTML = 'никтофобия или скотофобия (др.-греч. &laquo;ночь&raquo; и&nbsp;&laquo;страх&raquo;)&nbsp;&mdash; боязнь темноты, ночи. является самой распространённой фобией среди взрослых и&nbsp;детей. страх темноты вызывает галлюцинации и&nbsp;детям часто кажется, что кроме них в&nbsp;доме кто-то есть.'
+document.querySelector('.insidefear').style.marginTop = '20vw'
+document.querySelector('.light').style.display = 'block'
+document.querySelector('.light').style.marginBottom= '8vw'
+document.querySelector('.nightmare').style.display = 'none'
+}
+
+document.querySelector('.nightmarefear').onclick = function() {
+document.querySelector('.insidefear').innerHTML = 'гипнофобия (клинофобия или сомнифобия)&nbsp;&mdash; нерациональный и&nbsp;неконтролируемый страх перед сном. гипнофобия может происходить из&nbsp;страха потери контроля над происходящим, развиваться из&nbsp;ночных кошмаров.'
+document.querySelector('.insidefear').style.marginTop = '24vw'
+document.querySelector('.nightmare').style.display = 'block'
+document.querySelector('.nightmare').style.marginBottom= '8vw'
+document.querySelector('.light').style.display = 'none'
+}
